d46dff07f57dedf1fc7a7b56ee538553d68e5c1d
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-12-07 20:13:25 +0600

    Create Post API for Added book

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const cors = require("cors")
-const app = express()
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 3000
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 3000;
 
 // midleware
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bqrplnr.mongodb.net/?appName=Cluster0`;
 
@@ -19,15 +17,37 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("book_courier_db");
+    const bookCollection = db.collection("allbooks");
+
+
+    // All Books Api
+
+    app.get("/allbooks", (req, res) => {
+
+    });
+
+    app.post('/allbooks', async(req, res)=>{
+      const books = req.body
+      const result = await bookCollection.insertOne(books)
+      res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -35,11 +55,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Book Courier')
-})
+app.get("/", (req, res) => {
+  res.send("Book Courier");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
